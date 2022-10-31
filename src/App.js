b0376dae0613c04d7a9d1168feee1d5a5d74c765
Alessandro Pecorilla <Pecorillaalessandro@gmail.com>
2022-10-31 11:50:53 +0100

    #2 - Implemented the App component with routing.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
+import styled from 'styled-components'
+// Import of all pages from Index
+import { About, Cart, Checkout, Error, Home, Products, SingleProduct, Private } from './pages/index'
+
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    /* Router */
+    <Router>
+      {/* Navbar Component */}
+      <Navbar />
+      {/* Sidebar Component */}
+      <Sidebar />
+      {/* Switch */}
+      <Switch>
+        {/* Home */}
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        {/* About */}
+        <Route exact path='/about'>
+          <About />
+        </Route>
+        {/* Cart */}
+        <Route exact path='/cart'>
+          <Cart />
+        </Route>
+        {/* Products */}
+        <Route exact path='/products'>
+          <Products />
+        </Route>
+        {/* SingleProduct */}
+        <Route exact path='/products/:id' children={<SingleProduct />} />
+        {/* Checkout */}
+        <Route exact path='/checkout'>
+          <Checkout />
+        </Route>
+        {/* Error Page */}
+        <Route path='*'>
+          <Error />
+        </Route>
+
+
+      </Switch>
+      {/* Footer Component */}
+      <Footer />
+    </Router>
+  )
 }
 
 export default App
---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// Pages imported
+import About from './AboutPage'
+/* import AuthWrapper from './AuthWrapper' */
+import Cart from './CartPage'
+import Checkout from './CheckoutPage'
+import Error from './ErrorPage'
+import Home from './HomePage'
+import Products from './ProductsPage'
+import SingleProduct from './SingleProductPage'
+import Private from './PrivateRoute'
+
+
+
+export { About, Cart, Checkout, Error, Home, Products, SingleProduct, Private }
